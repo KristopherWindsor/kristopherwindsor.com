f1f0beb520811e55a7cb39c48f8ef639edb415d3
Kristopher Windsor <kristopher@pinger.com>
2016-10-01 22:34:09 -0700

    add src/heartbeat

---
+++ src/heartbeat/announce.php
@@ -0,0 +1,14 @@
+<?php
+
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
+$system = @$_POST['system'];
+@$ip = $_SERVER['REMOTE_ADDR'];
+
+if (!ctype_alpha($system) || strlen($system) > 30) $system = 'bad';
+
+file_put_contents($system . '.json', json_encode( array( 'date'=>date(DATE_ATOM), 'ip'=>$ip ) ));
+
+echo 'ok';
