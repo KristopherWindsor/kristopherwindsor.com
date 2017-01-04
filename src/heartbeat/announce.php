09d2c39f6cb4835252a5abfc566b4b831b3e779b
Kristopher Windsor <kristopher@pinger.com>
2017-01-03 20:50:53 -0800

    heartbeat - support GET so I can pass links around

--- src/heartbeat/announce.php
+++ src/heartbeat/announce.php
@@ -4,7 +4,7 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
-$system = @$_POST['system'];
+$system = @$_REQUEST['system'];
 @$ip = $_SERVER['REMOTE_ADDR'];
 
 if (!ctype_alpha($system) || strlen($system) > 30) $system = 'bad';
