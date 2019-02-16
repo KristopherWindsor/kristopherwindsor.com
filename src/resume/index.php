bf73865e9e88db874a3b559964f89bb8997e46cf
Kristopher Windsor <kristopher@pinger.com>
2019-02-15 17:53:38 -0800

    Resume should open in browser, not be downloaded

--- src/resume/index.php
+++ src/resume/index.php
@@ -2,7 +2,6 @@
 
 header('Content-Type: application/pdf');
 header('Cache-Control: public, max-age=3600');
-header('Content-Disposition: attachment; filename="kristopher-windsor.pdf"');
 
 readfile(__DIR__ . '/source/kristopher.pdf');
 
