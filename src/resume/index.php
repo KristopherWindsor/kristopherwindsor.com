d14be4b2b80506651dce95f93b9ccf511728f078
Kristopher Windsor <kristopher@pinger.com>
2019-02-15 17:44:31 -0800

    Remove markdown version of resume

--- src/resume/index.php
+++ src/resume/index.php
@@ -1,27 +1,8 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Kristopher's Resumé</title>
-    <link href="style.css" rel="stylesheet" type="text/css" title="Classic">
-    <link rel="shortcut icon" href="/favicon.png">
+<?php
 
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-    <meta name="description" content="Kristopher Windsor's resumé">
-    <meta name="keywords" content="kristopher windsor, kristopherwindsor, php, full-stack developer, backend developer, front-end developer, php developer">
-  </head>
-  <body>
-    <div id="box">
-      <?php
-      require_once __DIR__ . '/vendor/Parsedown.php';
-      $markdown = file_get_contents( __DIR__ . '/source/resume.markdown' );
-      $parsedown = new Parsedown();
-      echo $parsedown->text($markdown);
-      ?>
-    </div>
+header('Content-Type: application/pdf');
+header('Cache-Control: public, max-age=3600');
+header('Content-Disposition: attachment; filename="kristopher-windsor.pdf"');
 
-    <div id="download">
-      <a href="source/kristopher.pdf">View PDF</a> |
-      <a href="source/resume.markdown">Plain text</a>
-    </div>
-  </body>
-</html>
+readfile(__DIR__ . '/source/kristopher.pdf');
+
